ff95a73a9535df5fd4df5baade142225dad0987b
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2024-06-24 20:26:15 +0530

    Complete p02q02.

--- practical 02/q1.scala
+++ practical 02/q1.scala
@@ -1,15 +1,15 @@
 package  p2
 
 @main def question01() = 
-    val i, j, k = 2;
-    val m, n = 5;
+    val i, j, k = 2
+    val m, n = 5
     val f= 12.0f
-    val g = 4.0f;
-    val c = 'X';
+    val g = 4.0f
+    val c = 'X'
 
-    println(k + 12 * m);
-    println(m / j);
-    println(n % j);
-    println(m / j * j);
-    println(f + 10 * 5 + g);
+    println(k + 12 * m)
+    println(m / j)
+    println(n % j)
+    println(m / j * j)
+    println(f + 10 * 5 + g)
     // println(++i * n)
---
+++ practical 02/q2.scala
@@ -0,0 +1,12 @@
+package p2
+
+@main def question2() = 
+    val a = 2
+    val b = 3
+    val c =4
+    val d= 5
+    val k = 4.3f
+    val g = 4.0f
+
+    println(-2 * ( g - k ) +c);
+
