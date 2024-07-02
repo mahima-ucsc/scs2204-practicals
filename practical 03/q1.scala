efc2e5eedfaf0b93c2e3a99a0165c81196eb1d87
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2024-07-02 07:01:36 +0530

    Remove deprecated method.

--- practical 03/q1.scala
+++ practical 03/q1.scala
@@ -2,9 +2,9 @@ package p3
 
 def reversedString(str: String): String = str.length() match
     case 0 => ""
-    case strLength => str.charAt(strLength - 1) + reversedString(str.substring(0, strLength - 1))
+    case strLength => s"${str.charAt(strLength - 1)}${reversedString(str.substring(0, strLength - 1))}"
 
 
-@main def main() = 
+@main def q1() = 
     val str = "mahima"
     println(s"Reversed string = ${reversedString(str)}")
