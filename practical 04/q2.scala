f4b1f131eb479ef037b193aeb22b1072d00b1ebf
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2024-07-09 08:09:38 +0530

    Complete p04q02.

---
+++ practical 04/q2.scala
@@ -0,0 +1,12 @@
+package p4
+
+def checkInt(num: Int) = num match
+    case x if x <= 0 => println("Negative/Zero")
+    case x if x%2 == 0 => println("Even number")
+    case _ => println("Odd number")
+
+@main def q2() = 
+    println("Enter an Int: ")
+    val num = scala.io.StdIn.readInt()
+
+    checkInt(num)
