801dc884f3a7da25a8fd623f3d75f5648bb17c55
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2024-07-09 09:54:13 +0530

    Change q3 output.

--- practical 04/q3.scala
+++ practical 04/q3.scala
@@ -4,16 +4,15 @@ def toUpper(str: String): String = str.toUpperCase()
 def toLower(str: String): String = str.toLowerCase()
 def formatNames(f: String => String, str: String) = f(str)
 
-def printFormattedNames(names: List[String]): Unit = names match
-    case Nil => ()
-    case _ => {
-        println(names.head)
-        println(formatNames(toLower, names.head))
-        println(formatNames(toUpper, names.head))
-        println()
-        printFormattedNames(names.tail)
-    }
-
 @main def q3() = 
-    val names = List("BENNY", "NIroshan", "saman", "KumarA")
-    printFormattedNames(names)
+    val name1 = "Benny"
+    println(formatNames(toUpper, name1))
+
+    val name2 = "Niroshan"
+    println(s"${formatNames(toUpper, name2.substring(0, 2))}${formatNames(toLower, name2.substring(2))}")
+
+    val name3 = "Saman"
+    println(formatNames(toLower, name3))
+
+    val name4 = "Kumara"
+    println(s"${name4.substring(0, 5)}${formatNames(toUpper, name4.substring(5))}")
