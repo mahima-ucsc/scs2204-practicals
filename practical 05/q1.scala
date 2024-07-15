1c61d227846a61f1a7dcc5c918aadee5bc856c12
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2024-07-16 00:54:06 +0530

    Complete p05q01.

---
+++ practical 05/q1.scala
@@ -0,0 +1,40 @@
+package p5
+
+import scala.compiletime.ops.string
+
+def getProductList() : List[String]=
+    println("Enter product name: ")
+    val input = scala.io.StdIn.readLine()
+
+    input match
+        case "" => 
+            println("Product Name Cannot be Empty.")
+            getProductList()
+        case x if x.toLowerCase() == "done" => 
+            Nil
+        case _ => 
+            input :: getProductList()
+        
+def printProductList(productList : List[String]) : Unit =
+    def helper(productList : List[String], indexFromOne : Int): Unit =
+        productList match
+            case  Nil => ()
+            case _ =>
+                println(s"$indexFromOne: ${productList.head}")
+                helper(productList.tail, indexFromOne + 1)
+
+    helper(productList, 1)
+
+def getToatalProducts(productList: List[String]) : Int =
+    productList match 
+        case Nil => 0
+        case _ => 1 + getToatalProducts(productList.tail)
+
+@main def q1() = 
+    println("Enter the product list: ")
+    val productList = getProductList()
+
+    println("\n=== Product List ===")
+    printProductList(productList)
+
+    println(s"\nTotal products: ${getToatalProducts(productList)}")
