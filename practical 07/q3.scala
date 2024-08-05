a628213ea9661b03df2a3674c9164b8f710d9987
Mahima de Silva <2022cs033@stu.ucsc.cmb.ac.lk>
2024-08-06 02:22:44 +0530

    Update logic.

--- practical 07/q3.scala
+++ practical 07/q3.scala
@@ -1,11 +1,7 @@
 package p7
 
 def filterPrime(list: List[Int]) = 
-    list.filter(i => i match 
-        case x if x <= 1 => false
-        case 2 => true
-        case x => (2 to x-1).forall(x % _ != 0)
-    )
+    list.filter(num => (1 to num).filter(num % _ == 0).length == 2)
 
 @main def q3() =
     val numbers = (-50 to 50).toList
